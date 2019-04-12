f4922699ca410e22d2dd35765cd51df5033c59e6
joshinils <joshua.laskowski@web.de>
2019-04-12 11:04:31 +0200

    ws norm

--- CAD2/TestARX/TestARX.cpp
+++ CAD2/TestARX/TestARX.cpp
@@ -12,41 +12,41 @@
 #pragma warning( disable : 4512 )
 
 
-#include <aced.h> 
+#include <aced.h>
 #include <rxregsvc.h>
 #include "tchar.h"
 
-void TestARXCmd() 
-{ 
-    acutPrintf(_T("\nTestARX command!")); 
+void TestARXCmd()
+{
+    acutPrintf(_T("\nTestARX command!"));
 }
 
-void InitApp() 
-{ 
-    acedRegCmds->addCommand(_T("TESTARX_COMMANDS"), _T("TestARX"), _T("TesteARX"), 
-                            ACRX_CMD_TRANSPARENT, TestARXCmd); 
+void InitApp()
+{
+    acedRegCmds->addCommand(_T("TESTARX_COMMANDS"), _T("TestARX"), _T("TesteARX"),
+                            ACRX_CMD_TRANSPARENT, TestARXCmd);
 }
 
-void UnloadApp() 
-{ 
-    acedRegCmds->removeGroup(_T("TESTARX_COMMANDS")); 
+void UnloadApp()
+{
+    acedRegCmds->removeGroup(_T("TESTARX_COMMANDS"));
 }
 
-extern "C" AcRx::AppRetCode 
-acrxEntryPoint(AcRx::AppMsgCode msg, void* pkt) 
-{ 
-    switch (msg) 
-    { 
-        case AcRx::kInitAppMsg: 
-            acrxDynamicLinker->unlockApplication(pkt); 
-            acrxRegisterAppMDIAware(pkt); 
-            InitApp(); 
-            break; 
-        case AcRx::kUnloadAppMsg: 
-            UnloadApp(); 
-            break; 
-        default: 
-            break; 
+extern "C" AcRx::AppRetCode
+acrxEntryPoint(AcRx::AppMsgCode msg, void* pkt)
+{
+    switch (msg)
+    {
+        case AcRx::kInitAppMsg:
+            acrxDynamicLinker->unlockApplication(pkt);
+            acrxRegisterAppMDIAware(pkt);
+            InitApp();
+            break;
+        case AcRx::kUnloadAppMsg:
+            UnloadApp();
+            break;
+        default:
+            break;
     }
     return AcRx::kRetOK;
 }
